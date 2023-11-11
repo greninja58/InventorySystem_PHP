6787d90b1dc3a71668cb8de13d8aef2947319d9a
N1ckool <104719138+Div7195@users.noreply.github.com>
2023-11-12 03:37:11 +0530

    Added graphs

--- admin.php
+++ admin.php
@@ -88,7 +88,7 @@
          </strong>
        </div>
        <div class="panel-body">
-         <table class="table table-striped table-bordered table-condensed">
+         <table id = "highest_selling" class="table table-striped table-bordered table-condensed">
           <thead>
            <tr>
              <th>Title</th>
@@ -99,9 +99,9 @@
           <tbody>
             <?php foreach ($products_sold as  $product_sold): ?>
               <tr>
-                <td><?php echo remove_junk(first_character($product_sold['name'])); ?></td>
+                <td id = "name_highest"><?php echo remove_junk(first_character($product_sold['name'])); ?></td>
                 <td><?php echo (int)$product_sold['totalSold']; ?></td>
-                <td><?php echo (int)$product_sold['totalQty']; ?></td>
+                <td id = "qty_highest"><?php echo (int)$product_sold['totalQty']; ?></td>
               </tr>
             <?php endforeach; ?>
           <tbody>
@@ -118,7 +118,7 @@
           </strong>
         </div>
         <div class="panel-body">
-          <table class="table table-striped table-bordered table-condensed">
+          <table id="latest_sales" class="table table-striped table-bordered table-condensed">
        <thead>
          <tr>
            <th class="text-center" style="width: 50px;">#</th>
@@ -131,13 +131,13 @@
          <?php foreach ($recent_sales as  $recent_sale): ?>
          <tr>
            <td class="text-center"><?php echo count_id();?></td>
-           <td>
+           <td id = "name_latest">
             <a href="edit_sale.php?id=<?php echo (int)$recent_sale['id']; ?>">
              <?php echo remove_junk(first_character($recent_sale['name'])); ?>
            </a>
            </td>
            <td><?php echo remove_junk(ucfirst($recent_sale['date'])); ?></td>
-           <td>$<?php echo remove_junk(first_character($recent_sale['price'])); ?></td>
+           <td id = "sale_latest">$<?php echo remove_junk(first_character($recent_sale['price'])); ?></td>
         </tr>
 
        <?php endforeach; ?>
@@ -156,7 +156,7 @@
       </div>
       <div class="panel-body">
 
-        <div class="list-group">
+        <div id = "recently_added" class="list-group">
       <?php foreach ($recent_products as  $recent_product): ?>
             <a class="list-group-item clearfix" href="edit_product.php?id=<?php echo    (int)$recent_product['id'];?>">
                 <h4 class="list-group-item-heading">
@@ -165,8 +165,10 @@
                   <?php else: ?>
                   <img class="img-avatar img-circle" src="uploads/products/<?php echo $recent_product['image'];?>" alt="" />
                 <?php endif;?> -->
-                <?php echo remove_junk(first_character($recent_product['name']));?>
-                  <span class="label label-warning pull-right">
+                <span id = "name_recently">
+                  <?php echo remove_junk(first_character($recent_product['name']));?>
+                </span>
+                  <span id="price_recently" class="label label-warning pull-right">
                  $<?php echo (int)$recent_product['sale_price']; ?>
                   </span>
                 </h4>
@@ -175,11 +177,130 @@
               </span>
           </a>
       <?php endforeach; ?>
+      
     </div>
   </div>
  </div>
+ 
 </div>
+
  </div>
+ <canvas id="myChart1" style="width:100%;max-width:900px;margin-top:60px"></canvas>
+ <canvas id="myChart2" style="width:100%;max-width:900px;margin-left:60px;margin-top:60px"></canvas>
+ <canvas id="myChart3" style="width:100%;max-width:900px;margin-left:60px;margin-top:60px"></canvas>
+<script>
+let xValuesHighest = [];
+let yValuesHighest = [];
+let xValuesLatest = [];
+let yValuesLatest = [];
+let xValuesRecently = [];
+let yValuesRecently = [];
+let maxLatest = 0;
+let maxHighest = 0;
+let maxRecently = 0;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var table = document.getElementById("highest_selling");
+  var table2 = document.getElementById("latest_sales");
+    
+for (var i = 0, row; row = table.rows[i]; i++) {
+
+   for (var j = 0, col; col = row.cells[j]; j++) {
+     if(col.id === "name_highest"){
+      xValuesHighest.push(col.innerText);
+      
+     }else if(col.id === "qty_highest"){
+      yValuesHighest.push(col.innerText);
+      maxHighest = Math.max(yValuesHighest[yValuesHighest.length - 1], maxHighest);
+     }
+   }  
+}
+new Chart("myChart1", {
+  type: "line",
+  data: { 
+    labels: xValuesHighest,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValuesHighest
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 0, max:maxHighest}}],
+    }
+  }
+});
+
+for (var i = 0, row; row = table2.rows[i]; i++) {
+
+for (var j = 0, col; col = row.cells[j]; j++) {
+  if(col.id === "name_latest"){
+    xValuesLatest.push(col.innerText);
+   
+  }else if(col.id === "sale_latest"){
+  
+   yValuesLatest.push(col.innerText.substring(1, col.innerText.length));
+   maxLatest = Math.max(yValuesLatest[yValuesLatest.length - 1], maxLatest);
+  }
+}  
+}
+new Chart("myChart2", {
+  type: "line",
+  data: { 
+    labels: xValuesLatest,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValuesLatest
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 0, max:maxLatest}}],
+    }
+  }
+});
+
+
+var divE =document.getElementById("recently_added");
+for(let i = 0;i<divE.children.length;i++){
+  xValuesRecently.push(divE.children[i].querySelector('#name_recently').innerText);
+  yValuesRecently.push(divE.children[i].querySelector('#price_recently').innerText.substring(1, divE.children[i].querySelector('#price_recently').innerText.length));
+  maxRecently = Math.max(yValuesRecently[yValuesRecently.length - 1], maxRecently);
+}
+
+new Chart("myChart3", {
+  type: "line",
+  data: { 
+    labels: xValuesRecently,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValuesRecently
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 0, max:maxRecently}}],
+    }
+  }
+});
+
+});
+
+
+
+</script>
   <div class="row">
 
   </div>
